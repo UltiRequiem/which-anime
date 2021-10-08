e001678565715e141f16b78363a7287daa9d12c5
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-10-08 06:35:27 -0500

    :sparkles: Accept URLs

--- cli.ts
+++ cli.ts
@@ -1,15 +1,20 @@
 import { parse } from "https://deno.land/std@0.110.0/flags/mod.ts";
 import { blue, red } from "https://deno.land/std@0.110.0/fmt/colors.ts";
 
-function printHelp(color: (text: string) => string) {
-  console.log(color("TODO"));
+function printHelp() {
+  console.log(blue("TODO"));
 }
 
 function getFlags() {
   const { help, link, file } = parse(Deno.args);
 
   if (help) {
-    printHelp(blue);
+    printHelp();
+    Deno.exit(0);
+  }
+
+  if (!help && !link && !file) {
+    printHelp();
     Deno.exit(0);
   }
 
@@ -20,6 +25,37 @@ function getFlags() {
   return [link, file];
 }
 
-function main() {}
+async function main() {
+  const [link, file] = getFlags();
+
+  if (link) {
+    const request = await fetch(
+      `https://api.trace.moe/search?url=${encodeURIComponent(link)}`,
+    );
+
+    // Pass to external Type
+    const data: {
+      frameCount: number;
+      eror: string;
+      result: {
+        anilist: number;
+        filename: string;
+        episode: 1;
+        from: number;
+        to: number;
+        similarity: number;
+        video: string;
+        image: string;
+      }[];
+    } = await request
+      .json();
+
+    console.log(data.result[0]);
+  }
+
+  if (file) {
+    console.log("TODO");
+  }
+}
 
 main();
